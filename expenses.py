1b3477df7cca2be6521c3c9454ccbe353acb8a39
Benjamin Weigang <Benjamin.Weigang@student.uni-kassel.de>
2015-10-19 21:38:07 +0200

    Use decimal package, change_entry function, sort output by date

--- expenses.py
+++ expenses.py
@@ -4,6 +4,7 @@
 import os
 import shutil
 import argparse
+from decimal import Decimal, InvalidOperation
 from datetime import datetime
 from pydblite import Base
 
@@ -41,7 +42,7 @@ def add_entry(args):
     else:
         tags = []
 
-    db.insert(name=args.name, cost=args.cost, date=date, tags=tags)
+    db.insert(name=args.name, cost=Decimal(args.cost), date=date, tags=tags)
     db.commit()
 
 
@@ -60,6 +61,29 @@ def delete_entry(args):
         print('Deleted entry {}!'.format(args.id))
 
 
+def change_entry(args):
+    if args.col != 'tags':
+        assert len(args.val) == 1, 'Only one value for column "{}" possible.'.format(args.col)
+
+    if args.col == 'name':
+        val = args.val[0]
+    elif args.col == 'cost':
+        try:
+            val = Decimal(args.val[0])
+        except InvalidOperation:
+            raise InvalidOperation('Cannot convert "{}" to decimal.'.format(args.val[0]))
+    elif args.col == 'date':
+        try:
+            val = datetime.strptime(args.val[0], date_format)
+        except ValueError:
+            raise ValueError('Incorrect date format, should be "DD.MM.YYYY".')
+    elif args.col == 'tags':
+        val = args.val
+
+    db.update(db[args.id], **{args.col: val})
+    db.commit()
+
+
 def list_entries(args):
     print('')
     print('{:<20} {:<10} {:<12} {:<6} {:<20}'.format('Name', 'Cost[Euro]', 'Date', 'ID', 'Tags'))
@@ -71,8 +95,10 @@ def list_entries(args):
         days = 30
 
     total_cost = 0
-    for entry in (entry for entry in db if (today-entry['date']).days <= days):
-        if args.tags and not any(tag in entry['tags'] for tag in args.tags):
+    for entry in sorted([entry for entry in db if (today-entry['date']).days <= days], key=lambda x: x['date']):
+        if args.tags and not any(tag in entry['tags'] + [entry['name']] for tag in args.tags):
+            continue
+        if args.Tags and any(tag in entry['tags'] + [entry['name']] for tag in args.Tags):
             continue
         total_cost += entry['cost']
         print(entry_string(entry))
@@ -114,7 +140,7 @@ def main():
     parser_add = subparsers.add_parser('add', help='Add an entry to the data'
         ' base.')
     parser_add.add_argument('name', type=str, help='Name of the entry.')
-    parser_add.add_argument('cost', type=float, help='Cost of the entry.')
+    parser_add.add_argument('cost', type=str, help='Cost of the entry.')
     parser_add.add_argument('-d', '--date', type=str, help='Date of'
             ' expenditure. Usage "-d DD.MM.YYYY". Defaults to the current date.')
     parser_add.add_argument('-t', '--tags', type=str, nargs='+', help='Give a'
@@ -129,11 +155,25 @@ def main():
         ' deleted.')
     parser_delete.set_defaults(func=delete_entry)
 
+    parser_change = subparsers.add_parser('change', help='Change an entry in'
+        ' the database.')
+    parser_change.add_argument('id', type=int, help='ID of the entry to be'
+        ' changed.')
+    parser_change.add_argument('col', type=str, help='Column to be changed. Valid choices are'
+        ' "name", "cost", "date" and "tags".')
+    parser_change.add_argument('val', type=str, nargs='*', help='Value to be'
+        ' inserted. The tags list will get replaced completely.')
+    parser_change.set_defaults(func=change_entry)
+
     parser_list = subparsers.add_parser('list', help='List entries.')
     parser_list.add_argument('-d', '--days', type=int, help='Number of past'
         ' days the will be included in the output. Defaults to 30.')
     parser_list.add_argument('-t', '--tags', type=str, nargs='+', help='Only'
-        ' list entries with the specified tags. Usage: "-t tag1 tag2 ...".')
+        ' list entries with the specified tags. Entry names are treated as'
+        ' tags. Usage: "-t tag1 tag2 ...".')
+    parser_list.add_argument('-T', '--Tags', type=str, nargs='+', help='Only'
+        ' list entries without the specified tags. Entry names are treated as'
+        ' tags. Usage: "-T tag1 tag2 ...".')
     parser_list.set_defaults(func=list_entries)
 
     parser_setup = subparsers.add_parser('setup', help='Create the database in'
